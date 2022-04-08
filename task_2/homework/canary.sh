57655e2913446a6f8183ba22f1b235c8cfa4871c
Valentin-Kozlov <proxi10@yandex.ru>
2022-04-08 16:40:26 +0500

    add homework v3

--- task_2/homework/canary.sh
+++ task_2/homework/canary.sh
@@ -1,5 +1,11 @@
-for i in {1..15}
-do 
-test=$(curl --silent http://localhost:31865 | grep head)
+#!/bin/bash
+for i in {1..10}
+do
+
+# Проверка canary deployment ingress percent 
+test=$(curl --silent http://127.0.0.1 | grep head)
+
+# Проверка canary: always
+# test=$(curl -H "canary: always" --silent http://127.0.0.1 | grep head)
 echo $test
 done
